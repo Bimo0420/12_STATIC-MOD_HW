6c4175e067c2e96b1d16e174bc94dafe1e3fc442
Алексей Викулин <vaa@megamade.ru>
2022-08-06 22:32:22 +0300

    Add project files.

---
+++ N1/Cyrcle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N1
+{
+    static class Cyrcle
+    {
+        static public double GetL (double r)
+        {
+            return 2 * Math.PI*r;
+        }
+        static public double GetS(double r)
+        {
+            return Math.PI * r*r;
+        }
+        static public string ChekBelongPoint(double x, double y, double r, double x0, double y0)
+        {
+            if (r * r == (x - x0)*(x - x0) + (y - y0)*(y - y0))
+                return "Точка принадлежит окружности";
+            return "Точка не принадлежит окружности";
+        }
+    }
+}
---
+++ N1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N1 // Разработать статический класс для работы с окружностью. Класс должен содержать 3 метода:
+
+//метод, определяющий длину окружности по заданному радиусу;
+//метод, определяющий площадь круга по заданному радиусу;
+//метод, проверяющий принадлежность точки с координатами(x, y) кругу с радиусом r и координатами центра x0, y0.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = Convert.ToDouble(Console.ReadLine());
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            double y0 = Convert.ToDouble(Console.ReadLine());
+            double L = Cyrcle.GetL(r);
+            double S = Cyrcle.GetS(r);
+            string belong = Cyrcle.ChekBelongPoint(x, y, r, x0, y0);
+            Console.WriteLine(L);
+            Console.WriteLine(S);
+            Console.WriteLine(belong);
+            Console.ReadKey();
+        }
+    }
+}
